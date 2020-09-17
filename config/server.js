e848570597ab0ea84bd8a0dc560251f1e64ed572
Simon Rowe <simon.rowe@gmail.com>
2020-09-17 07:52:05 +0100

    Upgrade to strapi 3.1.4, add new skills, skills-groups, social-media. Add cv attribute to profile.

--- config/server.js
+++ config/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET') || "6D3HRUZuEPOHzpue9jwwlIwt1E5IPm8sTMYHlAYM4aEFBdh8JLrNnchj1u/RJf8buqptiCkhJGFmbgIn9dczxw==",
+    },
+  },
 });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'adb9b737-c2f2-4008-95ce-9fcc8a5ad43f'
+};
